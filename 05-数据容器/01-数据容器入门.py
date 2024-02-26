8658ff7b8c549951f90f3f8456d888b02e0d4b83
freedom2599 <390566161@qq.com>
2024-02-26 19:25:45 +0800

    学习

--- 05-数据容器/01-数据容器入门.py
+++
@@ -1,9 +0,0 @@
-"""
-演示python中关于数据容器的介绍
-"""
-# 数据容器：一种可以存储多个元素的python数据类型
-# 包括：list（列表），tuple（元组），str（字符串），set（集合），dict（字典）
-name_list = ['张三', '李四', '王五', '小刘', '小孟']
-print(name_list[0])
-print(name_list[-1])
-print(name_list[1])
